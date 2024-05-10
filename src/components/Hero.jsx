b0fcf85217d1dd4ab9857afb912974742ef829b4
sajjan kumar singh <sajjansingh1838@gmail.com>
2024-05-11 02:36:48 +0530

    button changes

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,16 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 const Hero = () => {
+ 
+  const navigate = useNavigate();
+
+
+  const handleClick = () => {
+    setTimeout(() => {
+      navigate("/recipes")
+    }, 200); // 1000 milliseconds = 1 second
+  };
   return (
     <div className="w-full h-[80vh] flex flex-col-reverse lg:flex-row justify-between items-center">
       <div className="left w-full lg:w-1/2 ">
@@ -15,7 +24,7 @@ const Hero = () => {
           similique quis, velit fugit, nemo unde consectetur voluptatem sequi
           repellat.
         </p>
-        <Link to={'/recipes'} className="relative inline-block text-lg group cursor-pointer	 ml-[12vh] lg:ml-0 mb-7">
+        <button onClick={handleClick} className="relative inline-block text-lg group cursor-pointer	 ml-[12vh] lg:ml-0 mb-7">
           <span className="relative z-10 block px-5 py-3 overflow-hidden font-medium leading-tight text-green-800 transition-colors duration-300 ease-out border border-gray-900 rounded-lg group-hover:text-white">
             <span className="absolute inset-0 w-full h-full px-5 py-3 rounded-lg bg-gray-50"></span>
             <span className="absolute left-0 w-48 h-48 -ml-2 transition-all duration-300 origin-top-right -rotate-90 -translate-x-full translate-y-12 bg-green-900 group-hover:-rotate-180 ease"></span>
@@ -25,7 +34,7 @@ const Hero = () => {
             className="absolute bottom-0 right-0 w-full h-12 -mb-1 -mr-1 transition-all duration-200 ease-linear bg-green-900 rounded-lg group-hover:mb-0 group-hover:mr-0"
             data-rounded="rounded-lg"
           ></span>
-        </Link>
+        </button>
       </div>
       <img
         className="lg:w-1/2 w-full"
